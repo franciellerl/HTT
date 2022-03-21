b99dd11c095e73f8ffd301724a42759551adbf46
franciellerl <101676930+franciellerl@users.noreply.github.com>
2022-03-20 21:39:24 -0300

    Create 10_Celsius.py

---
+++ 10_Celsius.py
@@ -0,0 +1,5 @@
+#10 Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+
+celsius = float(input("Digite a temperatura em graus Celsius: "))
+fahrenheit = ((celsius * 9) / 5) + 32
+print("A temperatura em graus Fahrenheit é ", fahrenheit)
