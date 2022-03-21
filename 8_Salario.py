3ea040c9372a4447674407b99bdac6e94dfe001c
franciellerl <101676930+franciellerl@users.noreply.github.com>
2022-03-20 21:38:03 -0300

    Create 8_Salario.py

---
+++ 8_Salario.py
@@ -0,0 +1,6 @@
+#8 Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+salariohora = float(input("Digite quanto você ganha por hora: "))
+numhora = int(input("Digite o número de horas que você trabalha por mês: "))
+salariototal = salariohora * numhora
+print("O total do seu salário é ", salariototal)
