e1d2319aa6410834186747159501c1c36454debe
franciellerl <101676930+franciellerl@users.noreply.github.com>
2022-03-20 21:34:03 -0300

    Create 3_Soma.py

---
+++ 3_Soma.py
@@ -0,0 +1,5 @@
+#3 Faça um Programa que peça dois números e imprima a soma.
+
+numero1, numero2 = map(int, input("Digite dois números: ").split())
+soma = numero1 + numero2
+print("A soma é ", soma)
