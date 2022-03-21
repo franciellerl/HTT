266032c90ec4feb5b21eef3748b336ae37a028c5
franciellerl <101676930+franciellerl@users.noreply.github.com>
2022-03-20 21:36:30 -0300

    Create 5_Metros.py

---
+++ 5_Metros.py
@@ -0,0 +1,5 @@
+#5 Faça um Programa que converta metros para centímetros.
+
+metros = float(input("Digite um número em metros: "))
+centimetros = metros * 100
+print("O número informado em centímetros é ", centimetros)
