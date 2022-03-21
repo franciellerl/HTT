e1cc16fd84c0db1c028e138133636fbc3ddc324e
franciellerl <101676930+franciellerl@users.noreply.github.com>
2022-03-20 21:37:29 -0300

    Create 7_Quadrado.py

---
+++ 7_Quadrado.py
@@ -0,0 +1,5 @@
+#7 Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+quadrado = float(input("Digite a medida de um dos lados do quadrado: "))
+dobro = (quadrado * quadrado) * 2
+print("O dobro da área do quadrado é ", dobro)
